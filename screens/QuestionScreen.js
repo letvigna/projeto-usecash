b88481460c14a912671a9a21e366dff57dc532ce
Letícia Vigna <leticia.vigna@hotmail.com>
2020-05-26 01:57:40 -0300

    Mudança para Class Component

--- screens/QuestionScreen.js
+++ screens/QuestionScreen.js
@@ -1,84 +1,89 @@
-import React, { useState, useReducer } from 'react';
-import { useFocusEffect } from '@react-navigation/native';
-import { View } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
 
 import Pergunta from '../components/Pergunta';
 import styles from '../services/styles';
 import questions from '../services/questions';
 
-export default function Perguntas({ navigation }) {
-  function reducer(state, action) {
-    console.log(state);
-    switch (action.type) {
-      case 'incrementPoints':
-        return { count: state.count, points: state.points + action.value };
-      case 'incrementCount':
-        return { count: state.count + 1, points: state.points };
-      case 'reset':
-        return { count: 0, points: 0 };
-      default:
-        throw new Error();
-    }
+export default class App extends Component {
+  constructor() {
+    super();
+    
+    this.state = {
+      count: 0,
+      points: 0,
+      currentQuestion: questions[0]
+    };
   }
 
-  const getQuestion = () => {
+  componentDidMount() {
+    this.setState({ currentQuestion: this.getQuestion() });
+  }
+
+  getQuestion() {
     let index;
-    if (state.count < 20) {
+    if (this.state.count < 20) {
       do {
         index = Math.floor(Math.random() * questions.length);
       } while (questions[index].isAnswered);
       questions[index].isAnswered = true;
     }
     return questions[index];
-  };
-
-  const [state, dispatch] = useReducer(reducer, { count: 0, points: 0 });
-  const [currentQuestion, setCurrentQuestion] = useState(getQuestion());
-
-  // Quando a tela de Perguntas estiver em foco
-  useFocusEffect(
-    React.useCallback(() => {
-      dispatch({ type: 'reset' });
-      setCurrentQuestion(getQuestion());
-    }, [])
-  );
-
-  const updatePoints = (add) => {
-    dispatch({ type: 'incrementPoints', value: add });
   }
 
-  const handleOnPress = (pressed, difficulty) => {
-    dispatch({ type: 'incrementCount' });
-    console.log("incrementCount dispatch ", state.count)
+  handleAnswerPress(pressed) {
+    this.incrementCount();
 
-    if (state.count < 19) {
+    if (this.state.count < 19) {
       if (pressed.isCorrect) {
-        switch (difficulty) {
+        switch (this.state.currentQuestion.type) {
           case 'E':
-            updatePoints(1);
+            this.incrementPoints(1);
             break;
           case 'M':
-            updatePoints(2);
+            this.incrementPoints(2);
             break;
           case 'H':
-            updatePoints(4);
+            this.incrementPoints(4);
             break;
         }
       }
-      setCurrentQuestion(getQuestion());
-    }
-    else {
-      navigation.navigate("EndScreen", { points: state.points });
-    }
-  };
 
-  return (
-    <View style={styles.container}>
-      <Pergunta
-        handler={handleOnPress}
-        pergunta={currentQuestion}
-        number={state.count + 1}
-      />
-    </View>
-  );
+      // Próxima pergunta
+      this.setState({
+        currentQuestion: this.getQuestion()
+      });
+    } else {
+      console.log("FIM :D")
+      console.log(this.state.points)
+      //this.props.navigation.navigate("EndScreen", { points: this.state.points })
+    }
+  }
+
+  incrementPoints(amount) {
+    this.setState({
+      points: this.state.points + amount
+    });
+  }
+
+  incrementCount() {
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Points: {this.state.points}</Text>
+        <Text>Count: {this.state.count}</Text>
+
+        <Pergunta
+          handler={this.handleAnswerPress.bind(this)}
+          pergunta={this.state.currentQuestion}
+          number={this.state.count + 1}
+        />
+      </View>
+    );
+  }
 }
