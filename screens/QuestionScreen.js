799b08ef81942d4531c05eb37cecb38e4e3cc9b9
Letícia Vigna <leticia.vigna@hotmail.com>
2020-05-26 11:49:28 -0300

    Remoção de Text das importações

--- screens/QuestionScreen.js
+++ screens/QuestionScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { View } from 'react-native';
 
 import Pergunta from '../components/Pergunta';
 import styles from '../services/styles';
